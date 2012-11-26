e0c93d3b47d54df592298a1d8b76b42ef7173b9b
Jason <jcdarwin@gmail.com>
2012-11-26 21:33:56 +1300

    Refactored js to be enscapulated modules

--- js/layout.js
+++ js/layout.js
@@ -4,37 +4,57 @@
 ** Author: Jason Darwin
 **
 ** Functions to support readk.it layout and navigation.
+**
+** Important concepts:
+** * page: a scrolling div containing an entire epub html file
 */
 
-// Global vars
-var scrollers = [];
-var page_width = 0;
-var iscroll = new iScroll('pageWrapper', {
-    snap: true,
-    momentum: false,
-    hScrollbar: false,
-    vScrollbar: false,
-    lockDirection: true});
+define([
+    'jquery',
+    'iscroll'
+], function($, iScroll){
 
-// Function to redraw the layout after DOM changes.
-function updateLayout() {
+    // Global vars
+    var scrollers = [];
+    var page_width = 0;
+    var iscroll = new iScroll('pageWrapper', {
+        snap: true,
+        momentum: false,
+        hScrollbar: false,
+        vScrollbar: false,
+        lockDirection: true});
 
-    var currentPage = 0;
+    // Function to redraw the layout after DOM changes.
+    var update = function () {
 
-    if (page_width > 0) {
-        currentPage = - Math.ceil( $('#pageScroller').position().left / page_width);
-    }
+        var currentPage = 0;
 
-    page_width = $('#pageWrapper').width();
-    var pages =$('.page').length;
-    $('#pageScroller').css('width', page_width * pages);
-    $('.page').css('width', page_width - 40);
-    iscroll.refresh();
-    iscroll.scrollToPage(currentPage, 0, 0);
-}
+        if (page_width > 0) {
+            currentPage = - Math.ceil( $('#pageScroller').position().left / page_width);
+        }
 
+        page_width = $('#pageWrapper').width();
+        var pages = $('.page').length;
+        $('#pageScroller').css('width', page_width * pages);
+        $('.page').css('width', page_width - 40);
+        iscroll.refresh();
+        iscroll.scrollToPage(currentPage, 0, 0);
+    };
 
-$(function() {
-    $(window).bind("orientationchange", updateLayout);
+    // Function to add a page
+    var add = function (id, html) {
+        $('#pageScroller').append('<div class="page"><div id="' + id + '" class="wrapper"><div class="scroller">' + html + '</div></div></div>');
+        scrollers.push( new iScroll(id, {hScrollbar: false, vScrollbar: false, lockDirection: true }) );
+        update();
+    };
+
+    $(function() {
+        $(window).bind("orientationchange", update);
+    });
+
+    return {
+        update: update,
+        add: add
+    };
+
 });
-
--- js/main.js
+++ js/main.js
@@ -1,39 +1,11 @@
-var epub_dir = "../html/mansfield_at_the_bay";
-
 requirejs.config({
     // specify our dependencies
     shim: {
-        'iscroll': ['jquery'],
-        'layout':  ['iscroll'],
-        'chrome':  ['layout'],
-        'epub_utilities': ['chrome']
+        'controller' : ['jquery', 'epub', 'iscroll', 'layout', 'chrome'],
+        'iscroll': {exports: 'iScroll'}
     }
 });
 
-require(["jquery", "iscroll", "layout", "chrome", "epub_utilities"], function($) {
-    $(function() {
-         $.get(epub_dir + '/META-INF/container.xml', {}, load_container);
-    });
+require(['controller'], function(controller){
+    controller.initialize();
 });
-
-function load_container(f) {
-    container(f, load_publication);
-}
-
-function load_publication(entries) {
-    console.log(entries);
-
-    $.each(entries, function(index, value){
-        console.log(value.href);
-        // Use the requirejs/text plugin to load our html resources.
-        // https://github.com/requirejs/text
-        require(["text!" + value.href + "!strip"],
-            function(html) {
-                $('#pageScroller').append('<div class="page"><div id="' + value.id + '" class="wrapper"><div class="scroller">' + html + '</div></div></div>');
-                updateLayout();
-                scrollers.push( new iScroll(value.id, {hScrollbar: false, vScrollbar: false, lockDirection: true }) );
-            }
-        );
-
-    });
-}

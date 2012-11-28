9da60d702d86548cc6a30dd43ab73d16f4891c74
Jason <jcdarwin@gmail.com>
2012-11-29 08:15:08 +1300

    Corrected test
    Former-commit-id: 8eabc3ac5d269629d31bb3719124a5f64745e56b
    Former-commit-id: 3e1788bf2b380f6bb1a1dfc9819f9e31e7c0e89b

--- js/test/app/epub.test.js
+++ js/test/app/epub.test.js
@@ -2,14 +2,32 @@ define(function (require) {
 
     require(['./controller', './config', './epub', './layout'], function(controller, config, epub, layout){
 
-        var initialized = function () {
-            QUnit.test("layout: contains correct number of pages, returns 13", function () {
-                QUnit.equal( $(layout.body).find('.page').size(), 13 );
-            });
+        // Refer http://benalman.com/talks/unit-testing-qunit.html for userful pointers
+
+        // Runs once after each assertion.
+        QUnit.log = function( result, message){
+            console.log((result ? "log" : "error" ) + (message ? message : ''));
         };
 
-        config.epub_dir = config.epub_directory;
-        controller.initialize(initialized);
+        module('mansfield', {
+            setup: function() {
+            }
+        });
+
+        test("layout: contains correct number of pages, returns 13", function () {
+            expect(1);
+
+            var initialized = function () {
+                start();
+                equal( $(layout.body).find('.page').size(), 13 );
+            };
+
+            config.epub_dir = config.epub_directory;
+            controller.initialize(initialized);
+            // Now we need to wait for the asynchronous callback to initialized
+            stop();
+
+        });
 
     });
 

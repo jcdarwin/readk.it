3306bebfe60bc5e4639331b5d4b5dfd94f410f2d
Jason <jcdarwin@gmail.com>
2012-12-02 23:07:13 +1300

    Support IDPF EPUB 3.0 samples in the html directory * Created manifest.maker.py * checked out IDPF EPUB 3.0 samples, but added entry to .gitignore to ensure they are not versioned * Refactor epub.test.js to cope with multiple books defined in the manifest

--- js/test/app/epub.test.js
+++ js/test/app/epub.test.js
@@ -1,6 +1,11 @@
+/*
+** QUnit test file.
+** Tests are driven by ../../html/mainfest.json
+*/
+
 define(function (require) {
 
-    require(['./controller', './config', './epub', './layout'], function(controller, config, epub, layout){
+    require(['./controller', './config', './epub', './layout'], function(Controller, config, epub, layout){
 
         // Refer http://benalman.com/talks/unit-testing-qunit.html for userful pointers
 
@@ -9,32 +14,67 @@ define(function (require) {
             console.log((result ? "log" : "error" ) + (message ? message : ''));
         };
 
-        config.epub_dir = config.epub_directory;
+        var data;
+        var epub_directory = config.epub_directory;
+        $.getJSON(epub_directory + 'manifest.json', function(d) {
+            data = d;
+        })
+        .success(function() {
+            $.each(data, function(index, book) {
+                perform_tests(book);
+            });
+        })
+        .error(function() { alert("error"); });
 
-        var data = {total_pages: 13};
 
-        module('mansfield', {
-            setup: function() {
-            }
-        });
+        function perform_tests(book) {
 
-        test("layout: contains correct number of pages, returns " + data.total_pages, function () {
-            expect(1);
+            module(book.name + " (EPUB " + book.version + ")", {
+                setup: function() {
+                }
+            });
 
             var initialized = function () {
-                start();
-                equal( $(layout.body()).find('.page').size(), data.total_pages );
 
-                $.each(controller.epub.getEntries(), function(index, value){
-                    console.log(value.id);
+                test("epub: contains correct number of pages, returns " + book.total_pages, function () {
+                    expect(1);
+                    equal(
+                        $(testController.getPublication().getToc()).size(),
+                        book.total_pages,
+                        'total number of pages = ' + $(testController.getPublication().getToc()).size()
+                    );
                 });
+
+                test("layout: contains correct number of pages, returns " + $(testController.getPublication().getToc()).size(), function () {
+                    expect(1);
+                    equal(
+                        $(layout.body()).find('.page').size(),
+                        $(testController.getPublication().getToc()).size(),
+                        'total number of pages = ' + $(layout.body()).find('.page').size()
+                    );
+                });
+
+                test("layout: contains pages in the order specified by opf file, return toc ids", function () {
+                    expect(1);
+                    deepEqual(
+                        $.map(testController.getPublication().getToc(), function(value){
+                            return value.id;
+                        }),
+                        $.map($(layout.body()).find('.page .wrapper'), function(value){
+                            return value.id;
+                        }),
+                        'order of pages is ' + $.map($(layout.body()).find('.page .wrapper'), function(value){
+                            return ' ' + value.id;
+                        })
+                    );
+                });
+
             };
 
             // Now we need to wait for the asynchronous callback to initialized
-            stop();
-            controller.initialize(initialized);
+            var testController = new Controller(epub_directory + book.path, initialized);
 
-        });
+        }
 
     });
 

3c1a94dc4b4bfd3289a757c6d2d77d33f5ade9aa
Jason <jcdarwin@gmail.com>
2012-12-18 23:05:54 +1300

    Added test files to test iframe usage

---
+++ test.manifest.js
@@ -0,0 +1,5 @@
+var manifest =
+[
+{"file": "html/thirdparty/30/accessible_epub_3/EPUB/ch02s02.xhtml", "name": "At the Bay", "version": "2.0", "identifier": "84ba3092-022a-460d-bb4f-fa558a022793", "path": "./mansfield_at_the_bay/", "total_pages": 13},
+{"file": "html/thirdparty/30/accessible_epub_3/EPUB/ch02s03.xhtml", "name": "Accessible EPUB 3", "version": "3.0", "identifier": "urn:isbn:9781449328030", "path": "./thirdparty/30/accessible_epub_3/", "total_pages": 22}
+];

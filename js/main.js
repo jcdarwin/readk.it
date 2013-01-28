eab554f9a4f7685c699f848cb9b2f31afdd8eda6
Jason <jcdarwin@gmail.com>
2013-01-28 22:49:14 +1300

    Library view calls to the reading view, using a query string parameter of 'path'
    Former-commit-id: 05198f3b5eb09fc863b00a0b1454b1777bc8e7b6
    Former-commit-id: 6632745d2ece62ab1e0ad29cac9d1eff729a8cf9

--- js/main.js
+++ js/main.js
@@ -13,7 +13,25 @@ var initialized = function () {
     console.log('main.initialized');
 };
 
-require(['app/controller', 'app/config'], function(Controller, config){
-    var book = {epub_directory: config.epub_directory, "name": "Accessible EPUB 3", "version": "3.0", "identifier": "urn:isbn:9781449328030", "path": "./thirdparty/30/accessible_epub_3/", "total_pages": 22};
+require(['jquery', 'app/controller', 'app/config'], function($, Controller, config){
+
+    // jquery plugin to allow us to parse query string arguments
+    $.fn.querystring = function (key) {
+        var re=new RegExp('(?:\\?|&)'+key+'=(.*?)(?=&|$)','gi');
+        var r=[], m;
+        while ((m=re.exec(document.location.search)) !== null) r.push(m[1]);
+        return r;
+    };
+
+    var book;
+    var path = $().querystring('path');
+
+    if (path) {
+        path = decodeURIComponent(path);
+        book = config.epub_directory + path;
+    } else {
+        //var book = {epub_directory: config.epub_directory, "name": "Accessible EPUB 3", "version": "3.0", "identifier": "urn:isbn:9781449328030", "path": "./thirdparty/30/accessible_epub_3/", "total_pages": 22};
+    }
+
     controller = new Controller(book, initialized);
 });

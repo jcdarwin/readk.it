3756240fb0303f12ae4042e2e9e13b5b7c4599c5
Jason <jcdarwin@gmail.com>
2013-06-18 22:10:14 +1200

    Refactor sass files, and only load grids for browsers, using enquire.js
    Former-commit-id: 8992e5f742db602c56797e93a97a293aa2cdfe7f
    Former-commit-id: 7024831245d977854320787f6836a868a4acd566

--- OEBPS/js/queries.js
+++ OEBPS/js/queries.js
@@ -1,55 +1,125 @@
 /* Our javascript-based media queries */
 /*global $:false */
 
-enquire.register("screen and (min-device-width : 768px) and (max-device-width : 1024px)", {
+$(document).ready(function() {
 
-    match : function() {
-        if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
-            // iBooks doesn't do columns, so revert our grid to a single column.
-            $('.pure-u-1-2').css('width', '100%');
+    /* ============== Browsers, not dedicated EPUB reading systems ============== */
+    enquire.register("screen", {
 
-            // Bump up the size of the body text a tad
-            $('p, li').css('font-size', '1.2rem');
+        match : function() {
+            if ( !navigator.epubReadingSystem) {
+                // mixin our grids (which make sense in a browser, but not in a dedicated EPUB reading system)
+                $('head').append('<link href="css/grids.css" media="screen, projection" rel="stylesheet" type="text/css" />');
 
-            // Hide the close button on callouts
-            $('.calloutNav').css({'height': '0', 'margin-bottom': '0', 'padding-top': '0'});
-            $('.calloutNavClose').css('display', 'none');
-
+                // Show the close button on callouts
+                $('.calloutNav').css({'height': '1.8rem', 'margin-bottom': '0.4rem', 'padding-top': '0.25rem'});
+                $('.calloutNavClose').css('display', 'block');
+            }
+        },
+        unmatch : function() {
         }
-    },
-    unmatch : function() {
-    }
 
-});
+    });
 
-enquire.register("screen and (min-device-width : 768px) and (max-device-width : 1024px) and (orientation : portrait)", {
+    /* ========================= Apple iBooks iPad ============================== */
+    enquire.register("screen and (min-device-width : 768px) and (max-device-width : 1024px)", {
 
-    match : function() {
-        if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
-            $('.container').css('width', '400px');
+        match : function() {
+            if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
+                // Ensure our headings stay with their next paragraph
+                $('h1, h2, h3, h4, h5, h6').css('page-break-after', 'avoid');
+
+                // Bump up the size of the body text a tad
+                $('p, li').css('font-size', '1.2rem');
+
+                $('.delta').css({'font-size': '26x'});
+                $('.delta').css({'font-size': '1.44444rem'});
+
+                // Resize the major headings to a sensible size
+                $('h1').css({'font-size': '48px'});
+                $('h1').css({'font-size': '2.66667rem'});
+
+                $('h2').css({'font-size': '36px'});
+                $('h2').css({'font-size': '2rem'});
+
+                $('.header').css({'padding-left': '10px', 'padding-right': '10px'});
+
+                // force page breaks
+                $('nav').css({'padding-top': '1rem'});
+                $('.pure-g-r').css({'margin-top': '4rem'});
+            }
+        },
+        unmatch : function() {
         }
-    },
-    unmatch : function() {
-        if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
-            $('.container').css('width', '320px');
-        }
-    }
 
-});
+    });
 
-enquire.register("screen and (min-device-width : 768px) and (max-device-width : 1024px) and (orientation : landscape)", {
+    enquire.register("screen and (min-device-width : 768px) and (max-device-width : 1024px) and (orientation : portrait)", {
 
-    match : function() {
-        if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
-            $('.container').css({'width': '320px', 'padding': '0 10px'});
-            $('h1').css('font-size', '2.66667rem');
+        match : function() {
+            if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
+                $('.container').css('width', '460px');
+            }
+        },
+        unmatch : function() {
+            if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
+                $('.container').css('width', '320px');
+            }
         }
-    },
-    unmatch : function() {
-        if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
-            $('.container').css({'width': '400px', 'padding': '0'});
-            $('h1').css('font-size', '3.33333rem');
+
+    });
+
+    // I really wish Apple would publish the details of the applicable media queries for iBooks
+    //enquire.register("screen and (min-device-width : 768px) and (max-device-width : 1024px) and (orientation : landscape)", {
+    enquire.register("screen and (width : 866px) and (orientation : landscape)", {
+
+        match : function() {
+            if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
+                $('.container').css({'width': '320px', 'padding': '0 10px'});
+                $('h1').css({'font-size': '32px'});
+                $('h1').css({'font-size': '1.77777rem'});
+                $('h2').css('page-break-before', 'always');
+            }
+        },
+        unmatch : function() {
+            if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
+                $('.container').css({'width': '460px', 'padding': '0'});
+                $('h1').css('page-break-after', 'auto');
+                $('h1').css({'font-size': '48px'});
+                $('h1').css({'font-size': '2.66667rem'});
+            }
         }
-    }
+
+    });
+
+    /* ======================== Apple iBooks iPhone ============================= */
+
+    /* iPhone 2G-4S in portrait & landscape */
+    enquire.register("screen and (min-device-width : 320px) and (max-device-width : 480px)", {
+        match : function() {
+            if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
+                $('.header-icon').css('width', '50%');
+                $('.header').css('padding-top', '1.5rem');
+                $('.container').css('width', '260px');
+                $('h1').css({'margin-bottom': '4rem'});
+                $('h2').css({'page-break-before': 'always'});
+                $('nav').css({'padding-top': '4rem'});
+            }
+        }
+    });
+
+    /* iPhone 5 in portrait & landscape */
+    enquire.register("screen and (min-device-width : 320px) and (max-device-width : 568px)", {
+        match : function() {
+            if ( navigator.epubReadingSystem && navigator.epubReadingSystem.name === 'iBooks') {
+                $('.header-icon').css('width', '50%');
+                $('.header').css('padding-top', '1.5rem');
+                $('.container').css('width', '260px');
+                $('h1').css({'margin-bottom': '4rem'});
+                $('h2').css({'page-break-before': 'always'});
+                $('nav').css({'padding-top': '4rem'});
+            }
+        }
+    });
 
 });

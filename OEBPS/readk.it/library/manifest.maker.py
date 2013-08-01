3cbbf4d1d57f1afa2a575e61d9681ea0e9d64a82
Jason <jcdarwin@gmail.com>
2013-08-02 00:01:46 +1200

    Switch manifest behaviour back to including OEBPS
    Former-commit-id: af2af77f6646070a9f53cd4cd077e8e445ac6957
    Former-commit-id: a9b16eb781aa80d97a5e0f094a22b64adf9eb1d6

--- OEBPS/readk.it/library/manifest.maker.py
+++ OEBPS/readk.it/library/manifest.maker.py
@@ -80,11 +80,11 @@ for r, d, files in os.walk("."):
             #For our special case, where we want to use Readk.it to both be used embedded in an
             #EPUB file (http://localhost:8000/OEBPS/readk.it/) and also to serve the library
             #(http://localhost:8000/OEBPS/readk.it/library/library.html)
-            #path = 'OEBPS/readk.it/library/' + re.sub(r'^(?:./)?(.*)(META-INF)$', r"\g<1>", r)
+            path = 'OEBPS/readk.it/library/' + re.sub(r'^(?:./)?(.*)(META-INF)$', r"\g<1>", r)
 
             #For the normal case, where we only want to serve the library
             # (http://localhost:8000/readk.it/library/library.html)
-            path = 'readk.it/library/' + re.sub(r'^(?:./)?(.*)(META-INF)$', r"\g<1>", r)
+            #path = 'readk.it/library/' + re.sub(r'^(?:./)?(.*)(META-INF)$', r"\g<1>", r)
             cover = opf.xpath("//opf:item[contains(@properties, 'cover-image')]/@href", namespaces=namespaces)
             if not cover:
                 coverid = opf.xpath("//opf:meta[contains(@name, 'cover')]/@content", namespaces=namespaces)

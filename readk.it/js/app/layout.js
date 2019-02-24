f1aa3be769c9b3e02d6b116ae55ef6f297b81344
jdarwin <jason@mobi2go.com>
2019-02-24 17:08:08 +1300

    Remove console.log

--- readk.it/js/app/layout.js
+++ readk.it/js/app/layout.js
@@ -100,7 +100,6 @@ define([
             }
 
             if (this.options['page_scroller_waiting']) {
-console.log(this.options);
                 this.options['page_scroller_waiting'].scroller.scrollToElement($('[id="' + this.options['page_scroller_anchor'] + '"]')[0], 0);
                 this.options['page_scroller_waiting'] = undefined;
                 this.options['page_scroller_anchor'] = undefined;
